88ffbca03aa9ed8403cf5532b895d35ca8dc87cc
emeliahughes <emeliahughes@live.com>
2024-04-24 14:39:26 -0400

    post first scrape results

--- scraping/scrape.sh
+++ scraping/scrape.sh
@@ -14,15 +14,10 @@ endDates=("20230131" "20230228" "20230331" "20230430" "20230531" "20230630" "202
 
 length=${#startDates[@]}
 length=$((length))
-# length=(1)
-
-#videoIDArray=() 
 videoIDString=""
 
 for line in $(cat videos.txt); do
-    #videoIDArray+=("$line")
     videoIDString+='"'$line'", '
-    #echo "line: $line"
 done < videos.txt
 
 videoIDString=${videoIDString:0:${#videoIDString}-2} 
@@ -44,8 +39,6 @@ do
             "end_date": "'"${endDates[$i]}"'"
         }')
 
-    #result=('{"data":{"has_more":false,"videos":[],"cursor":0},"error":{"code":"ok","message":"","log_id":"202404040125543266351757D5BF073438"}}')
-    #result=('{"error":{"code":"ok","message":"","log_id":"202404040125543266351757D5BF073438"}}')
     echo "RESULT"
     hasAnswer=$(jq -r '.data.videos' <<< ${result}) 
 
@@ -67,33 +60,5 @@ do
         fi
     fi
 
-    #jq -r '.videos.[].id' "videoInfo.json" | sort > resultIDs.txt
+    jq -r '.videos.[].id' "videoInfo.json" | sort > resultIDs.txt
 done
-
-
-
-
-# videoIDTest="7341796912883862814"
-# test=$(curl -X POST \
-#         'https://open.tiktokapis.com/v2/research/video/query/?fields=id,create_time,username,video_description,like_count,comment_count,share_count,view_count' \
-#         -H "authorization: bearer ${access_token}" \
-#         -d '{ 
-#         "query": {
-#             "and": [
-#                { "operation": "IN", "field_name": "video_id", "field_values": ["'${videoIDTest}'"] }
-#             ]
-#         }, 
-#             "start_date": "20240301",
-#             "end_date": "20240328"
-#         }')
-
-# testData=$(jq -r '.data.videos' <<< ${test})
-# testData=${testData:1:${#testData}-2}
-# testData = $testData | fromjson
-
-# if [[ -z "${testData[@]}" ]]; then
-#     echo "no result"
-#   else
-#     newFile=$(jq --argjson newVideo "$testData" '.videos += [$newVideo]' videoInfo.json)
-#     echo $newFile > videoInfo.json
-# fi
